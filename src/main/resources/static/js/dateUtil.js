10d4f55abfc81a93e707ac8158d9ebaa7048ad95
Qinjia.Liu <qinjia.liu@bestwehotel.com>
2017-03-09 17:45:57 +0800

    日期工具完善

--- src/main/resources/static/js/dateUtil.js
+++ src/main/resources/static/js/dateUtil.js
@@ -1,29 +1,71 @@
 Date.prototype.format = function(format) {
-    var date = {
-           "M+": this.getMonth() + 1,
-           "d+": this.getDate(),
-           "h+": this.getHours(),
-           "m+": this.getMinutes(),
-           "s+": this.getSeconds(),
-           "q+": Math.floor((this.getMonth() + 3) / 3),
-           "S+": this.getMilliseconds()
-    };
-    if (/(y+)/i.test(format)) {
-           format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-    }
-    for (var k in date) {
-           if (new RegExp("(" + k + ")").test(format)) {
-                  format = format.replace(RegExp.$1, RegExp.$1.length == 1
-                         ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
-           }
-    }
-    return format;
+	var date = {
+		"M+" : this.getMonth() + 1,
+		"d+" : this.getDate(),
+		"h+" : this.getHours(),
+		"m+" : this.getMinutes(),
+		"s+" : this.getSeconds(),
+		"q+" : Math.floor((this.getMonth() + 3) / 3),
+		"S+" : this.getMilliseconds()
+	};
+	if (/(y+)/i.test(format)) {
+		format = format.replace(RegExp.$1, (this.getFullYear() + '')
+				.substr(4 - RegExp.$1.length));
+	}
+	for ( var k in date) {
+		if (new RegExp("(" + k + ")").test(format)) {
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k]
+					: ("00" + date[k]).substr(("" + date[k]).length));
+		}
+	}
+	return format;
+};
+$('.form_datetime').datetimepicker({
+	language : 'zh-CN',//中文，需要引用zh-CN.js包
+	weekStart : 1,
+	todayBtn : 1,
+	autoclose : 1,
+	format : "yyyy-mm-dd hh:ii:ss",
+	beforeShow : getCurrentTime,
+	todayHighlight : 1,
+	startView : 2,
+	forceParse : 0,
+	showMeridian : 1
+});
+$('.form_date').datetimepicker({
+	language : 'zh-CN',
+	weekStart : 1,
+	todayBtn : 1,
+	format : 'yyyy-mm-dd',
+	autoclose : 1,
+	beforeShow : getCurrentTime,
+	todayHighlight : 1,
+	startView : 2,
+	minView : 2,
+	forceParse : 0
+});
+$('.form_time').datetimepicker({
+	language : 'zh-CN',
+	weekStart : 1,
+	todayBtn : 1,
+	autoclose : 1,
+	format : "hh:ii:ss",
+	beforeShow : getCurrentTime,
+	todayHighlight : 1,
+	startView : 1,
+	minView : 0,
+	maxView : 1,
+	forceParse : 0
+});
+
+function getCurrentTime() {
+	$(".ui-datepicker-current").click();
 }
-$(function(){
-	$('#datetimepicker').datetimepicker();
+
+$(function() {
 	//时间截转换日期
-	$('#times_tamp_to_times_date').click(function(){
-		var times_tamp=$('#times_tamp').val();
+	$('#times_tamp_to_times_date').click(function() {
+		var times_tamp = $('#times_tamp').val();
 		times_tamp = times_tamp.replace(/(^\s*)|(\s*$)/g, "");
 		if (/^\d{10}$/.test(times_tamp)) {
 			times_tamp *= 1000;
@@ -39,51 +81,57 @@ $(function(){
 	});
 
 	//日期转换时间截
-	$('#times_date_to_times_tamp').click(function(){
-		var times_date2=$('#times_date2').val();
+	$('#times_date_to_times_tamp').click(function() {
+		var times_date2 = $('#times_date2').val();
 		var timestamp2 = Date.parse(new Date(times_date2));
-		$('#times_tamp2').val(timestamp2/1000);
+		$('#times_tamp2').val(timestamp2 / 1000);
 	});
 
 	//日期时间差
-	$('#start_end_date').click(function(){
-		var date1=new Date($('#start_date').val());  //开始时间
-		var date2=new Date($('#end_date').val());    //结束时间
-		var date3=date2.getTime()-date1.getTime()  //时间差的毫秒数
+	$('#start_end_date').click(function() {
+		var date1 = new Date($('#start_date').val()); //开始时间
+		var date2 = new Date($('#end_date').val()); //结束时间
+		var date3 = date2.getTime() - date1.getTime() //时间差的毫秒数
 
 		//计算出相差天数
-		var days=Math.floor(date3/(24*3600*1000))
+		var days = Math.floor(date3 / (24 * 3600 * 1000))
 
 		//计算出小时数
-		var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数
-		var hours=Math.floor(leave1/(3600*1000))
+		var leave1 = date3 % (24 * 3600 * 1000) //计算天数后剩余的毫秒数
+		var hours = Math.floor(leave1 / (3600 * 1000))
 		//计算相差分钟数
-		var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
-		var minutes=Math.floor(leave2/(60*1000))
+		var leave2 = leave1 % (3600 * 1000) //计算小时数后剩余的毫秒数
+		var minutes = Math.floor(leave2 / (60 * 1000))
 
 		//计算相差秒数
-		var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
-		var seconds=Math.round(leave3/1000)
+		var leave3 = leave2 % (60 * 1000) //计算分钟数后剩余的毫秒数
+		var seconds = Math.round(leave3 / 1000)
 
-		$('#result_day').val(days+'天');
+		$('#result_day').val(days + '天');
 	});
 
 	//前几天
-	$('#js_old_date').click(function(){
-		var old_date=$('#old_date').val();
-		var result_old_date=moment().subtract(old_date, 'days').calendar();
-		$('#result_old_date').val(result_old_date);
+	$('#js_old_date').click(function() {
+		var currentDate = new Date($('#start_date').val());
+		var old_date_count = $('#old_date').val();
+		var timeStamp=currentDate.valueOf()-old_date_count*24*60*60*1000;
+		var result_old_date = new Date();
+		result_old_date.setTime(parseInt(timeStamp));
+		$('#result_old_date').val(result_old_date.format("yyyy-MM-dd"));
 	});
 
 	//后几天
-	$('#js_new_date').click(function(){
-		var new_date=$('#new_date').val();
-		var result_new_date=moment().add(new_date, 'days').calendar();
-		$('#result_new_date').val(result_new_date);
+	$('#js_new_date').click(function() {
+		var currentDate = new Date($('#start_date').val());
+		var new_date_count = $('#new_date').val();
+		var timeStamp=currentDate.valueOf()+new_date_count*24*60*60*1000;
+		var result_new_date =new Date();
+		result_new_date.setTime(parseInt(timeStamp));
+		$('#result_new_date').val(result_new_date.format("yyyy-MM-dd"));
 	});
 
 	var newDate = new Date();
-	var now_date=newDate.format('yyyy-MM-dd');
+	var now_date = newDate.format('yyyy-MM-dd');
 	$('#start_date').val(now_date);
 	$('#end_date').val(now_date);
 });
